02fba5d2d23fc608507849cac8a8ff04158303a5
wbChang <2692685036@qq.com>
2020-01-15 11:35:36 +0800

    更改配置2

--- src/main/java/com/albert/bootlaunch/Controller/HelloController.java
+++ src/main/java/com/albert/bootlaunch/Controller/HelloController.java
@@ -1,4 +1,4 @@
-package com.albert.bootlaunch;
+package com.albert.bootlaunch.Controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
