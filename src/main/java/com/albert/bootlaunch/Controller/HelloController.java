885aa1eb61cb2909e4d82dfb1bc8b7d0921a2a8b
wbChang <2692685036@qq.com>
2020-02-08 10:08:50 +0800

    test

--- src/main/java/com/albert/bootlaunch/Controller/HelloController.java
+++ src/main/java/com/albert/bootlaunch/Controller/HelloController.java
@@ -1,7 +1,7 @@
 package com.albert.bootlaunch.Controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+        import org.springframework.web.bind.annotation.RequestMapping;
+        import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
